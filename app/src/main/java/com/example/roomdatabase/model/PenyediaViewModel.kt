6ad81acb02234185a1c4c5dbf0b91e917e0540f2
Nur Azizah <nurrrazizahhh11@gmail.com>
2023-12-05 23:08:14 +0700

    menambahkan val Factory di PenyediaViewModel

--- app/src/main/java/com/example/roomdatabase/model/PenyediaViewModel.kt
+++ app/src/main/java/com/example/roomdatabase/model/PenyediaViewModel.kt
@@ -1,4 +1,24 @@
 package com.example.roomdatabase.model
 
+import android.text.Spannable.Factory
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.roomdatabase.ui.theme.AplikasiSiswa
+
 class PenyediaViewModel {
+    val Factory = viewModelFactory {
+
+        initializer {
+            HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+        initializer {
+            EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+    }
 }
+
+fun CreationExtras.aplikasiSiswa():AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
