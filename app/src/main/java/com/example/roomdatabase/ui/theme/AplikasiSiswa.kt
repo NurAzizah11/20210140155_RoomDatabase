a3355ad21a8ed03842b66a53db6afca6092279f4
Nur Azizah <nurrrazizahhh11@gmail.com>
2023-12-05 23:03:32 +0700

    membuat file AplikasiSiswa lalu menambahkan isinya

---
+++ app/src/main/java/com/example/roomdatabase/ui/theme/AplikasiSiswa.kt
@@ -0,0 +1,15 @@
+package com.example.roomdatabase.ui.theme
+
+import android.app.Application
+import com.example.roomdatabase.repositori.ContainerApp
+import com.example.roomdatabase.repositori.ContainerDataApp
+
+class AplikasiSiswa : Application() {
+
+    lateinit var container: ContainerApp
+
+    override fun onCreate() {
+        super.onCreate()
+        container = ContainerDataApp(this)
+    }
+}
