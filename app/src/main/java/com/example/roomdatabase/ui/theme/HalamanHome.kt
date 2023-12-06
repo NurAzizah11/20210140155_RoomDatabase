5cbfdf90504df13cfa366b977a6b8fd75b29e44b
Nur Azizah <nurrrazizahhh11@gmail.com>
2023-12-06 11:13:48 +0700

    menambahkan class object DestinasiHome

--- app/src/main/java/com/example/roomdatabase/ui/theme/HalamanHome.kt
+++ app/src/main/java/com/example/roomdatabase/ui/theme/HalamanHome.kt
@@ -1,4 +1,14 @@
 package com.example.roomdatabase.ui.theme
 
-object HalamanHome {
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.roomdatabase.R
+import com.example.roomdatabase.model.HomeViewModel
+import com.example.roomdatabase.navigasi.DestinasiNavigasi
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
 }
